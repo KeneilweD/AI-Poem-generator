bb6f196a6b4ec1c68dc646103ee2d8ec7fc1b650
KeneilweD <keneilwed.mohohlo@gmail.com>
2025-04-30 17:42:45 +0200

    added api intergration and diplayed the users input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-
-  poemElement.innerHTML = "  Within you are everything";
 }
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "b0o0f604484c421bt63fad88c200d0d5";
+  let context =
+    "you are romantic poems expert and love to write short poems,your missionn is to write a four line poem, do not include the title of the poem in basic HTML.make sure to follow the user instructions Sign the poem with 'Neilwe' at the end of the poem in  <em>";
+  let prompt = `User instruction : Generate a poem about ${instructionInput.value} `;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
